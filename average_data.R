9b5921754368c24b1fc6173222c69a172a281dc6
vrramya <vrramya@users.noreply.github.com>
2015-01-25 16:26:12 -0800

    Update average_data.R

--- average_data.R
+++ average_data.R
@@ -1,6 +1,4 @@
 library(reshape2)
-
-# Creates a data set with the average of each variable for each activity and each subject
 AverageData <- function(dataset) {
   molten <- melt(dataset, id.vars=c("subject", "activity"))
   dcast(molten, subject + activity ~ variable, mean)
