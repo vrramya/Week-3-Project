b22644aa750b9398cd359d78d7502eb99499a184
vrramya <vrramya@users.noreply.github.com>
2015-01-25 16:26:48 -0800

    Update download_data.R

--- download_data.R
+++ download_data.R
@@ -1,6 +1,3 @@
-# Retrieves Human Activity Recognition data set
-# and saves it to ./UCI HAR Dataset, unless the
-# ./UCI HAR Dataset exists already.
 DownloadData <- function() {
   if (!file.exists('./UCI HAR Dataset')) {
     file.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
