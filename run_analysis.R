a58a148b7e4f319defbcffed7eb4ef2633816bd5
vrramya <vrramya@users.noreply.github.com>
2015-01-25 16:11:17 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,10 +2,9 @@ source("download_data.R")
 source("merge_data.R")
 source("average_data.R")
 
-# Runs the full analysis, downloading the data if needed,
-# merging the test and train datasets, and generating a
-# new dataset containing the average of each variable
-# for each activity on each subject. Outputs the new
+# Runs the full analysis
+# merging the test and train datasets
+#Outputs the new
 # dataset at ./averages.txt
 RunAnalysis <- function() {
   DownloadData()
